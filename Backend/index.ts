18d102e6234f94c78c1f49c3ba53306dbec5a2cb
Abhishek Pawar <abhishekpawar131202@gmail.com>
2025-05-16 23:41:07 +0530

    Fix casing in file paths and update main entry point in package.json

--- Backend/index.ts
+++ Backend/index.ts
@@ -32,10 +32,10 @@ app.use("/api/v1/restaurant", restaurantRoute);
 app.use("/api/v1/menu", menuRoute);
 app.use("/api/v1/order", orderRoute);
 
-app.use(express.static(path.join(DIRNAME, "/frontend/dist")));
+app.use(express.static(path.join(DIRNAME, "/Frontend/dist")));
 
 app.use("*", (_, res) => {
-    res.sendFile(path.resolve(DIRNAME, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(DIRNAME, "Frontend", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
