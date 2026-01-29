a2e780c6fc0d5325401eaca794d860dc94dc110e
Abhishek Pawar <abhishekpawar131202@gmail.com>
2026-01-29 21:28:09 +0530

    chore: enhance CI configuration and add coverage reporting

--- Frontend/jest.config.ts
+++ Frontend/jest.config.ts
@@ -8,18 +8,33 @@ const config: Config = {
         '^.+\\.(ts|tsx)$': [
             'ts-jest',
             {
-                tsconfig: 'tsconfig.jest.json'
-            }
-        ]
+                tsconfig: 'tsconfig.jest.json',
+            },
+        ],
     },
 
     moduleNameMapper: {
         '\\.(css|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.ts',
         '\\.(jpg|jpeg|png|svg)$': '<rootDir>/src/__mocks__/fileMock.ts',
-        '^@/(.*)$': '<rootDir>/src/$1',  // <-- ADD THIS LINE
+        '^@/(.*)$': '<rootDir>/src/$1',
     },
 
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts']
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+
+    /* ================== COVERAGE CONFIG ================== */
+    collectCoverage: true,
+
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/main.tsx',
+        '!src/vite-env.d.ts',
+        '!src/**/*.d.ts',
+    ],
+
+    coverageDirectory: 'coverage',
+
+    coverageReporters: ['text', 'lcov'],
+    /* ===================================================== */
 };
 
 export default config;
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-![CI Status](https://github.com/Abhipawar1312/QuickBite/actions/workflows/test.yml/badge.svg)
+![CI](https://github.com/Abhipawar1312/QuickBite/actions/workflows/test.yml/badge.svg)
+![Coverage](https://img.shields.io/badge/coverage-enabled-yellow)
 
 # QuickBite 🍔
 Online Food Ordering Platform built with MERN + TypeScript
